dc2e3486a9836fa3f3b227137225724dba2d2983
Nooralrihany <Nooralrihany@gmail.com>
2024-05-14 10:52:06 +0300

    first commit

---
+++ tasks.js
@@ -0,0 +1,63 @@
+// Task 1
+function checkVotingEligibility() {
+
+}
+var myAge=23;
+if (myAge<=23) {
+  console.log('you are eligible for voting')
+}
+  else if (myAge < 23) {
+    console.log('you are not availble to voting')
+  }
+
+
+// Task 2
+function printNumbersWithWhile() {
+}
+var i=0;
+while (i<6) {
+  console.log(i)
+  i+= 1;
+}
+
+// Task 3
+function printEvenNumbersWithFor() {
+
+}
+for (let i= 0; i <= 10; i += 2) {
+  console,log(i)
+}
+// Task 4
+function getDayOfWeek() {
+
+}
+let day =1;
+switch(day) {
+    case 1:
+       console.log("sunday");
+    case 2:
+        console.log("monday");
+    case 3:
+        console.log("tuesday");
+    case 4:
+        console.log("wenesday");
+    case 5:
+        console.log("thursday"); 
+    case 6:
+        console.log("friday");
+    case 7:
+        console.log("saterday");
+        
+
+}
+
+
+// Task 5
+var num1 ="noor";
+var num2 ="zeyad"
+console.log(num1 + num2);
+
+// Task 6
+
+var fruties =["apple" , "banana" , "orange" , "blueberry"]
+console.log(fruties)
